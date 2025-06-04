b0dff030df80f30ba71f6e023dbb69ec655ae463
DasHenning <h.h.reckentin@icloud.com>
2025-06-04 22:24:52 +0200

    full Playermovement and capture to screen

--- player.py
+++ player.py
@@ -23,14 +23,29 @@ class Player(cs.CircleShape):
         pygame.draw.polygon(surface, "white", self.triangle())
 
     def update(self, dt):
+        # locking player to screen pac-man style
+        if self.position.x + 20 < 0:
+            self.position.x = constants.SCREEN_WIDTH + 20
+        elif self.position.x - 20 > constants.SCREEN_WIDTH:
+            self.position.x = -20
+        if self.position.y < 0:
+            self.position.y = constants.SCREEN_HEIGHT
+        elif self.position.y > constants.SCREEN_HEIGHT:
+            self.position.y = 0
+        
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
             self.rotate(-dt) 
         if keys[pygame.K_d]:
             self.rotate(dt)
-
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
     def rotate(self, dt):
         self.rotation += dt*constants.PLAYER_ROTATE_SPEED
         self.rotation %= 360  # Keep the rotation within 0-360 degrees
     
-    
+    def move(self, dt):
+        self.velocity = pygame.Vector2(0, -1).rotate(self.rotation) * constants.PLAYER_SPEED * dt
+        self.position += self.velocity
